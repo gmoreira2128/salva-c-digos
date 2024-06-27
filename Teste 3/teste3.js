3a8eb6d9e6a745f99fb89ce86b00d1b140a87fa2
Gabriel Moreira <133922923+gmoreira2128@users.noreply.github.com>
2024-06-27 16:25:19 -0300

    Update teste3.js

--- Teste 3/teste3.js
+++ Teste 3/teste3.js
@@ -1,4 +1,9 @@
 document.getElementById('btn-Sign-up').addEventListener('click', function() {
+    let input = document.getElementById('nome');
+    document.getElementById('sob-nome');
+    document.getElementById('email');
+    document.getElementById('senha');
+    document.getElementById('CPF');
     let cadastro = document.querySelector('.cad');
     if (cadastro.style.display === 'none' || cadastro.style.display === '') {
         cadastro.style.display = 'flex'; // Mostra a div de cadastro
@@ -7,19 +12,20 @@ document.getElementById('btn-Sign-up').addEventListener('click', function() {
         cadastro.style.left = '1100px';
         cadastro.style.width = '400px';
         cadastro.style.height = '500px';
+        input.style.padding = '10px';
     } else {
         cadastro.style.display = 'none'; // Esconde a div de cadastro
+        input.style.padding = '0px';
     }
 });
 
 document.getElementById('btn-Sign-in').addEventListener('click', function(){
-    let Input = document.querySelector('.Log .input');
     let Login = document.querySelector('.Log');
     if(Login.style.display == 'none' || Login.style.display == ''){
         Login.style.display = 'flex';
         Login.style.flexDirection = 'column';
         Login.style.alignItems = 'center';
-        Login.style.left = '1100px'
+        Login.style.left = '1100px';
         Login.style.width = '400px';
         Login.style.height = '500px';
         Login.style.padding = '30px';
@@ -27,4 +33,18 @@ document.getElementById('btn-Sign-in').addEventListener('click', function(){
     } else{
         Login.style.display = 'none';
     }
+});
+
+document.getElementById('Pricing').addEventListener('mouseover', function() {
+    let precos = document.querySelectorAll('#PRECO');
+    precos.forEach(function(preco) {
+        preco.style.display = 'block';
+    });
+});
+
+document.getElementById('Pricing').addEventListener('mouseout', function() {
+    let precos = document.querySelectorAll('#PRECO');
+    precos.forEach(function(preco) {
+        preco.style.display = 'none';
+    });
 });
